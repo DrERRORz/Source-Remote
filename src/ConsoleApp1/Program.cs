ac78a5344630c3762690e6bfa0659cd468d2d374
Trler <ceo_of_sex1@protonmail.com>
2022-08-03 15:26:58 -0500

    .

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using XDevkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.Test.Xbox.XDRPC;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        static XboxConsole myConsole;
+
+        static uint GetTitleID()
+        {
+            // invoke RPC by ordinal (DLL or Module export Number)
+            // Since xbox 360 does not export by name like on other platforms
+            // we must instead invoke by ordinal number
+            //
+            // xam.xex (xbox aux methods) module contains a function at ordinal number 463
+            // that returns the currently running game ID (number)
+            //
+            // our RPC is constructed as follows
+            // XDRPCMode = Title (system should work too)
+            // module = xam.xex
+            // ordinal number = 463
+            // arguments = empty object
+            return myConsole.ExecuteRPC <uint>(XDRPCMode.Title, "xam.xex", 463);
+        }
+
+        static void OpenConnection()
+        {
+            while (true)
+            {
+                XboxManagerClass xbm = new XboxManagerClass();
+                string dflt_cnsl = xbm.DefaultConsole;
+                Console.Write($"{dflt_cnsl}\nIs this the correct address? Y/N: ");
+                var userInput = Console.ReadLine();
+                if (userInput.ToUpper() == "Y")
+                {
+                    try
+                    {
+                        Console.WriteLine("\nConnecting...\n");
+                        XboxManager manager = (XDevkit.XboxManager)Activator.CreateInstance(Type.GetTypeFromCLSID(new Guid("A5EB45D8-F3B6-49B9-984A-0D313AB60342")));
+                        myConsole = manager.OpenConsole(dflt_cnsl);
+                    }
+                    catch
+                    {
+                        Console.Write("Failed connection!\nRetrying...\n");
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"Connected to {myConsole.Name}");
+                        Console.WriteLine($"current process name: {myConsole.RunningProcessInfo.ProgramName}\n");
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nReissuing request...\n");
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RemoteConsole RmtCnsl = new RemoteConsole(myConsole);
+            OpenConnection();
+
+            var titleID = GetTitleID();
+
+            // switch running title id
+            switch (titleID)
+            {
+                case 0x4541080f:
+                    Console.WriteLine("The Orange Box is running!");
+                    break;
+                default:
+                    break;
+            }
+            var exitCmd = "allahahkbar";
+            Console.Write("Connected! \n\nEnter Command: ");
+            var lastCmd = Console.ReadLine();
+            RmtCnsl.AddCommand(lastCmd,myConsole);
+
+
+            while (lastCmd != exitCmd)
+            {
+                // do rpc if not exit constant
+            }
+            Console.WriteLine("bye");
+            
+        }
+        
+    }
+}
---
+++ src/ConsoleApp1/RemoteConsole.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Test.Xbox.XDRPC;
+using XDevkit;
+
+namespace ConsoleApp1
+{
+	public class RemoteConsole
+	{
+
+		private XboxConsole _myConsole;//  --- Please get back to this and figure out how to sync with main class
+		//
+		// 1. create constructor taking XboxConsole as a parameter (store as private member) -
+		// 2. Create 2 public methods -
+		//		AddCommand -
+		//		ExecuteCommands -
+		// 3. in AddCommand (CBuf_AddText)
+		//		create a call using the XboxConsole to Execute an RPC
+		//			i.e. myConsole.ExecuteRPC(XDRPCMode.Title, [address we got in IDA], { });
+		// 4. do the same in ExecuteCommands (CBuf_Execute)
+		//
+		// Part 2:
+		// 1. Move OpenConnection and GetTitleID from main method into this class as private methods
+		// 2. Change constructor to take in nothing
+		// 3. change constructor to call OpenConnection
+		//
+		// Part 3:
+		// 1. add new constructor taking a string called 'consoleName' which can be the IP or Name of the console
+		// 2. add overload to OpenConnection which explicitly opens by name/IP
+		// 3. implement IDisposable on this class (hint: if you do RemoteConsole : IDisposible
+		//		visual studio will popup and ask if you want it to auto-populate interface members following IDispose pattern
+		// 4. Dispose of XboxConsole object (CloseConnection and possibly dispose() if it implements IDisposable.
+
+		public RemoteConsole(XboxConsole myConsole)
+        {
+			//XboxConsole cnsl;
+			//cnsl = myConsole;
+
+			// correct way
+			//this._myConsole = myConsole;
+        }
+
+		public void AddCommand(/*String - use primitive types (blue ones)*/ string command, XboxConsole myConsole) ///Please solve issue with the myConsole and make sync with main class without arg
+        {
+			uint cbufAddr = 0x860992A0;
+            try
+            {
+                myConsole.ExecuteRPC<uint>(XDRPCMode.Title, cbufAddr, new object[] { command });
+            }
+            catch
+            {
+				Console.WriteLine("\nFailed to insert command!\n");
+            }
+            finally
+            {
+				Console.WriteLine("Executing command!");
+				ExecuteCommands();
+			}
+            // mode, address, arguments (array)
+            //myConsole.ExecuteRPC<uint>(XDRPCMode.Title, , argInfo);
+
+		}
+
+		public void ExecuteCommands()
+        {
+			uint cbufAddr = 0x8609B670;
+			_myConsole.ExecuteRPC<uint>(XDRPCMode.Title, cbufAddr, new object[] {});
+		}
+	}
+}
